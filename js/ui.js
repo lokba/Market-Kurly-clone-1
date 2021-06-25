025284233d7839ba96f78c2b6718cd0dbe193111
sanglok <evertree6031@gmail.com>
2021-06-25 15:44:20 +0900

    header > 배송지 등록 dropdown 기능 추가

--- js/ui.js
+++ js/ui.js
@@ -1,5 +1,7 @@
 let menu_info = document.querySelector("#info");
 let subinfo = document.querySelector(".subinfo");
+let location_img = document.querySelector("#location");
+let location_subinfo = document.querySelector(".location_subinfo");
 
 menu_info.addEventListener("mouseover", () => {
     subinfo.style.display = "block";
@@ -7,5 +9,13 @@ menu_info.addEventListener("mouseover", () => {
 
 menu_info.addEventListener("mouseout", () => {
     subinfo.style.display = "none";
+})
+
+location_img.addEventListener("mouseover", () => {
+    location_subinfo.style.display = "block";
+})
+
+location_img.addEventListener("mouseout", () => {
+    location_subinfo.style.display = "none";
 })
 
